39113c94732ba31a7db6b261288cff65a155ce00
Virace <Virace@aliyun.com>
2024-03-16 15:56:09 +0800

    :tada: tada~

---
+++ riotmanifest/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2024/3/16 15:28
+# @Update  : 2024/3/16 15:28
+# @Detail  : 
